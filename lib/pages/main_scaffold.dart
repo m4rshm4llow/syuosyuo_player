ca7d2ef85930c4c6914f7d8b1ac0623fbf352d06
ましまろ <marshxmallow222@gmail.com>
2025-04-14 00:28:27 +0900

    🎉 first commit

---
+++ lib/pages/main_scaffold.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:syuosyuo_player/pages/mobile_scaffold.dart';
+import 'package:syuosyuo_player/provider/data_provider.dart';
+import 'package:syuosyuo_player/router.dart';
+import 'package:syuosyuo_player/theme/widgets/theme_mode_button.dart';
+import 'package:url_launcher/link.dart';
+
+class MainScaffold extends StatelessWidget {
+  const MainScaffold({super.key, required this.navigationShell});
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    final breakpoint = ResponsiveBreakpoints.of(context).breakpoint;
+    return switch (breakpoint.name) {
+      MOBILE || TABLET => MobileScaffold(navigationShell),
+      (_) => PcScaffold(navigationShell),
+    };
+  }
+}
+
+class PcScaffold extends StatelessWidget {
+  const PcScaffold(this.navigationShell, {super.key});
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: const NavigationTitle(),
+        centerTitle: false,
+        elevation: 4,
+        actions: [
+          ThemeModeButton(),
+          IconButton(
+            onPressed: () {
+              SearchPageRoute().go(context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          const Gap(8),
+        ],
+      ),
+      body: Row(
+        children: [
+          _Drawer(),
+          VerticalDivider(thickness: 1, width: 1, color: colorScheme.primary.withAlpha(30)),
+          Expanded(child: navigationShell),
+        ],
+      ),
+    );
+  }
+}
+
+enum NavigationItem {
+  dashboard(iconData: Icons.dashboard_outlined),
+  users(iconData: Icons.list_outlined);
+
+  const NavigationItem({required this.iconData});
+  final IconData iconData;
+  String get label => name;
+}
+
+class NavigationTitle extends StatelessWidget {
+  const NavigationTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SelectionContainer.disabled(
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        child: Visibility(
+          visible: ResponsiveBreakpoints.of(context).largerThan(MOBILE),
+          child: GestureDetector(
+            onTap: () => const HomePageRoute().go(context),
+            child: Text(
+              'しゅおしゅおプレイヤー',
+              style: theme.textTheme.bodyLarge!.copyWith(fontWeight: FontWeight.w700),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Drawer extends HookConsumerWidget {
+  const _Drawer();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final links = ref.watch(dataProvider).valueOrNull?.links ?? [];
+    final lastUpdatedAt = ref.watch(dataProvider).valueOrNull?.lastUpdated ?? '';
+
+    return Container(
+      width: 240,
+      padding: const EdgeInsets.all(16),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          ListTile(
+            title: Text('歌枠一覧'),
+            onTap: () {
+              AchivesPageRoute().go(context);
+            },
+          ),
+          ListTile(
+            title: Text('歌唱曲一覧'),
+            onTap: () {
+              SongPageRoute().go(context);
+            },
+          ),
+          ListTile(
+            title: Text('アーティスト一覧'),
+            onTap: () {
+              ArtistsPageRoute().go(context);
+            },
+          ),
+          ListTile(
+            title: Text('サムネイルギャラリー'),
+            onTap: () {
+              ThumbnailPageRoute().go(context);
+            },
+          ),
+          ListTile(
+            title: Text('ツール'),
+            onTap: () {
+              ToolPageRoute().go(context);
+            },
+          ),
+          Divider(height: 1.0),
+          for (final link in links)
+            Link(
+              uri: Uri.parse(link.url),
+              target: LinkTarget.blank,
+              builder: (context, followLink) {
+                return ListTile(title: Text(link.title), onTap: followLink);
+              },
+            ),
+          Divider(height: 1.0),
+          ListTile(
+            title: Text('ライセンス'),
+            onTap: () {
+              showLicensePage(context: context);
+            },
+          ),
+          ListTile(
+            title: Text('クレジット'),
+            onTap: () {
+              CreditPageRoute().go(context);
+            },
+          ),
+          if (lastUpdatedAt.isNotEmpty) ...[
+            Divider(height: 1.0),
+            ListTile(title: Text('最終更新日'), subtitle: Text(lastUpdatedAt.formatDate())),
+          ],
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/watch_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:syuosyuo_player/provider/data_provider.dart';
+import 'package:youtube_player_iframe/youtube_player_iframe.dart';
+
+class WatchScreen extends HookConsumerWidget {
+  const WatchScreen({required this.videoId, super.key});
+  final String videoId;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final songs = ref.watch(songListProvider(videoId));
+    final controller = useMemoized<YoutubePlayerController>(() {
+      return YoutubePlayerController.fromVideoId(
+        videoId: videoId,
+        // NOTE: 自動再生を有効にしてしまうと元動画の再生数にカウントされないようなので注意
+        autoPlay: false,
+        params: const YoutubePlayerParams(
+          showControls: true,
+          captionLanguage: 'ja',
+          interfaceLanguage: 'ja',
+        ),
+      );
+    }, [videoId]);
+
+    return Scaffold(
+      body: Column(
+        children: [
+          ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: MediaQuery.sizeOf(context).width * 0.75),
+            child: YoutubePlayer(controller: controller),
+          ),
+          Gap(8),
+          Expanded(
+            child: ListView.builder(
+              itemCount: songs.length,
+              itemBuilder: (context, index) {
+                final song = songs[index];
+                return ListTile(
+                  title: Text(song.title),
+                  subtitle: Text(song.artist),
+                  trailing: Text(song.time),
+                  onTap: () {
+                    controller.seekTo(seconds: song.time.toSeconds(), allowSeekAhead: true);
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+extension StringX on String {
+  double toSeconds() {
+    List<String> parts = split(':');
+    if (parts.length != 3) {
+      throw FormatException('Invalid time format. Expected HH:MM:SS');
+    }
+
+    double hours = double.parse(parts[0]);
+    double minutes = double.parse(parts[1]);
+    double seconds = double.parse(parts[2]);
+
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+}
