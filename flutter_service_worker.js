e9b8b04dbb62c55a47ea5ba94bab984292fd1e79
m4rshm4llow <m4rshm4llow@users.noreply.github.com>
2025-04-14 15:27:42 +0000

    deploy: 3e7b9c4ffff1259598bb5a88bee0695ae0ec9f4a

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -35,12 +35,12 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "version.json": "ffd2aa48cd8adea78e3a0f97153ffae5",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
 "favicon.ico": "916dea7e9e97abccc99a85aa4b5e21f5",
-"main.dart.js": "5310738ab05d8ce6f879015ddee25bf8",
+"main.dart.js": "805b5e81050be5f298a7751019af0c31",
 "404.html": "8d7aedd3cc8694d16bcb8226b8fa5e27",
 "icons/Icon-192.png": "a2d5de070957d7b8e0d8c5763c207a92",
 "icons/Icon-512.png": "e9ca66534ed4b36bf3a91179f2add68a",
 "manifest.json": "b9dcba73bd8286303137bd44a49586f7",
-"flutter_bootstrap.js": "2272c76f996de649592d66a6a3d119a0"};
+"flutter_bootstrap.js": "fcce1bac6970f314a502080265b01f31"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
