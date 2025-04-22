a6829ca8a50c3b543c27b58e7865a0d7d0fe7eaf
m4rshm4llow <m4rshm4llow@users.noreply.github.com>
2025-04-22 12:51:20 +0000

    deploy: 887cb804a09439f697203f12f70cf400599ae510

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "assets/assets/images/mochi_syuo.webp": "5ae2fcc09d23aeed043bd6eb31c07ba2",
 "assets/assets/images/mochi_syuo_drooling.webp": "ccdd050ab00b43eac689ff2c04581410",
 "assets/assets/images/mochi_syuo_smiling.webp": "6c4ba2fb6a0606fbc18d032b4040dbb4",
-"assets/assets/json/data.json": "82c875552363a9608f6d7ffd1865d55a",
+"assets/assets/json/data.json": "80391c2784b1835e8c8c0dcbb38628ea",
 "assets/assets/license/OFL.txt": "f63c7614e3d3c61afd6176cfe0e0ba89",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "db2514507eff5641f0fa59bde9f0b77f",
@@ -40,7 +40,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "f2ad9363618c5f62e813740099a80e63",
 "icons/Icon-192.png": "a2d5de070957d7b8e0d8c5763c207a92",
 "icons/Icon-512.png": "e9ca66534ed4b36bf3a91179f2add68a",
 "manifest.json": "e949832eaa609781fc1ad73ff0a23c3a",
-"flutter_bootstrap.js": "1d26e656ab258f86e53876475e30002b"};
+"flutter_bootstrap.js": "dc2c3c870bfc79772b1f133820494673"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
