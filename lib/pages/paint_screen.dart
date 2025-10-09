119afbe56333528fdcf3ae9c1a93c004d7a3287d
ましまろ <marshxmallow222@gmail.com>
2025-10-10 08:02:07 +0900

    🎨 カラーピッカーを追加

--- lib/pages/paint_screen.dart
+++ lib/pages/paint_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:image_downloader_web/image_downloader_web.dart';
@@ -18,6 +19,35 @@ class PaintScreen extends HookConsumerWidget {
         title: const Text('アちゃん塗り絵ツール'),
         actions: [
           IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: const Text('色を選択'),
+                    content: SingleChildScrollView(
+                      child: ColorPicker(
+                        pickerColor: controller.color,
+                        onColorChanged: (Color color) {
+                          controller.setColor(color);
+                        },
+                        enableAlpha: false,
+                        pickerAreaHeightPercent: 0.8,
+                      ),
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.palette),
+          ),
+          IconButton(
             icon: Icon(Icons.save_alt),
             onPressed: () async {
               final image = await controller.exportImage();
@@ -30,7 +60,6 @@ class PaintScreen extends HookConsumerWidget {
           ),
         ],
       ),
-
       body: ImagePainter.network(
         'https://pbs.twimg.com/media/G1JntFhawAA81GZ?format=jpg&name=4096x4096',
         controller: controller,
